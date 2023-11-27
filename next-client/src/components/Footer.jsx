681c696cb5c990ffe78dc7dd17e7e62b74578104
조성원 <tjddnjsjo@kakao.com>
2023-11-28 03:02:38 +0900

    feat: Footer 컴포넌트 구현

---
+++ next-client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>
+          Copyright © 2023 - All right reserved by 경기대학교 분산및병렬처리 3팀
+        </p>
+      </aside>
+    </footer>
+  );
+}
