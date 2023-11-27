c388bcdf0183cc83ce21bae52cbe250f3dcf25a2
조성원 <tjddnjsjo@kakao.com>
2023-11-28 03:24:29 +0900

    refactor: 아이콘 수정

--- next-client/src/app/page.js
+++ next-client/src/app/page.js
@@ -1,5 +1,7 @@
+import { HiOutlineCheckCircle, HiOutlineXCircle } from 'react-icons/hi2';
+
 export default function Home() {
-  const isConnected = false;
+  const isConnected = true;
 
   return (
     <main className="prose w-full flex flex-col mx-auto">
@@ -7,36 +9,12 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         {isConnected ? (
           <div role="alert" className="alert alert-success">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <HiOutlineCheckCircle size={24} />
             <span>메타마스크에 연결되었습니다.</span>
           </div>
         ) : (
           <div role="alert" className="alert alert-error">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <HiOutlineXCircle size={24} />
             <span>메타마스크 연결에 실패했습니다.</span>
           </div>
         )}
--- next-client/src/components/Navbar.jsx
+++ next-client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import { HiBars3CenterLeft } from 'react-icons/hi2';
+
 export default function Navbar() {
   return (
     <div className="flex justify-center w-full bg-base-100">
@@ -7,20 +9,7 @@ export default function Navbar() {
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+              <HiBars3CenterLeft size={24} />
             </div>
             <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
               <li>
